69bee363bf1cb5d1457274a82afb1da0cb29dfef
humphray legare <humphray.legare@libeo.com>
2020-02-12 12:25:47 -0500

    init

---
+++ constants/Colors.js
@@ -0,0 +1,22 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  //humphray's define theme
+  mainColor: #AF74B3,
+  mainColorSecond: #C87ACC,
+  mainColorThird:#FFEDB3,
+  mainColorFourth:#CCFFF5,
+  mainColorFifth:#B3A26B,
+
+};
